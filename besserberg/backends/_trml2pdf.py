20f6ebe40494d0b28a79496374654bdb50cb4f52
Erik Jankovic <erik.jankovic@vnet.eu>
2017-02-24 14:43:08 +0100

    Create DejaVuSans family from registered fonts
    Signed-off-by: Erik Jankovic <erik.jankovic@vnet.eu>

--- besserberg/backends/_trml2pdf.py
+++ besserberg/backends/_trml2pdf.py
@@ -13,17 +13,27 @@ class Trml2PdfBackend(BesserbergBackend):
     BACKEND_CODE = 'trml2pdf'
 
     RML_FONTS = (
-        # (<name>, <path>)
         ('DejaVuSans', 'DejaVuSans.ttf'),
         ('DejaVuSans-Bold', 'DejaVuSans-Bold.ttf'),
         ('DejaVuSans-Italic', 'DejaVuSans-Oblique.ttf'),
         ('DejaVuSans-BoldItalic', 'DejaVuSans-BoldOblique.ttf'),
     )
 
-    def __init__(self):
+    RML_FONT_FAMILIES = {
+        'DejaVuSans': {
+            'normal': 'DejaVuSans',
+            'bold': 'DejaVuSans-Bold',
+            'italic': 'DejaVuSans-Italic',
+            'boldItalic': 'DejaVuSans-BoldItalic',
+        }
+    }
 
+    def __init__(self):
         for font in self.RML_FONTS:
             pdfmetrics.registerFont(TTFont(*font))
+
+        for family_name, family_fonts in self.RML_FONT_FAMILIES.items():
+            pdfmetrics.registerFontFamily(family_name, **family_fonts)
 
     def render(self, template, options=None):
         return trml2pdf.parseString(template)
